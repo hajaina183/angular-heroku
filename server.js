a35681850d0fb0a1f12046756f63b55e79cb4c66
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-02-26 19:25:58 +0300

    mises à jour à déployer sur heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+app.use(express.static('./dist/angular-heroku'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}
+  );
+});
+const NODE_PORT = process.env.PORT||5000;
+app.listen(NODE_PORT , function() {
+    console.log("node app is running at localhost : "+NODE_PORT);
+});
